3f5d64d5adcf5cae93ea482697ff489fded8dd2a
devRabbani <dev.golamrabbani@gmail.com>
2022-08-01 23:59:12 +0530

    Added feature activation of sytem

--- src/pages/Webniar/index.jsx
+++ src/pages/Webniar/index.jsx
@@ -1,9 +1,35 @@
 import './webniar.style.css'
+import { motion } from 'framer-motion'
+
+const mainvariants = {
+  hidden: {
+    x: -200,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      opacity: { duration: 0.7, ease: 'easeInOut' },
+      duration: 0.3,
+    },
+  },
+  exit: {
+    x: '100vw',
+    transition: { ease: 'easeInOut' },
+  },
+}
 
 export default function Webniar() {
   return (
-    <div className='wrapper webniar'>
+    <motion.div
+      className='webniar'
+      variants={mainvariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
+    >
       <p>No Webniar Details</p>
-    </div>
+    </motion.div>
   )
 }
